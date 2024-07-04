1d60ab514f3b26f61c9ed678300bae72c540fcf1
Kamal Nayan <geniusamansingh@gmail.com>
2024-07-04 17:20:55 +0530

    hardhat contracts

---
+++ dchain-dex-contracts/README.md
@@ -0,0 +1,17 @@
+# Dchain DEX contracts
+
+## Compiling the contract
+
+Use the following command to compile the contract:
+
+```bash
+npx hardhat build
+```
+
+## Deploying the contract
+
+Use the following command to deploy the contract to any EVM chain:
+
+```bash
+npx hardhat deploy
+```
---
+++ dchain-dex-contracts/hardhat.config.js
@@ -0,0 +1,17 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
